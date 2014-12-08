c1c20ef4cc1f2588f6ff4d927fad07116995e7bb
denisesh <denisesh@gmail.com>
2014-12-08 10:37:13 -0800

    create action views complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,18 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(params.require(:product).permit(:name, :description, :price))
+    @product.save
+    redirect_to root_path
+  end
+
+
+
 end
